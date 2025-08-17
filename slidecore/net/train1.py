0f577fb862e8dba3b450f2fbceadedc689dc1678
shimon-home2021 <82209573+shimon-home2021@users.noreply.github.com>
2025-08-17 11:44:25 +0300

    adding new hcf std2mean

--- slidecore/net/train1.py
+++ slidecore/net/train1.py
@@ -44,7 +44,8 @@ def train_epoch(net=None, loader=None, optim=None, loss_obj=None, device=None):
         labs = labs.reshape((N,))
         loss = loss_obj(outputs, labs)
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=slideapp.config.clip_grad_val)
+        if slideapp.config.clip_grad_val>0:
+            torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=slideapp.config.clip_grad_val)
         optim.step()
         train_loss += loss.item()
         _,preds = outputs.max(1)
